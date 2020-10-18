d19a52a4d7ca888a90ac22d6a365b0532311a2be
Daniel <ddunn40495@gmail.com>
2020-10-18 16:31:20 -0400

    Database Connected, Table set, trying to fix rewrite on apache

--- api/models/props.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO listings
-    (address, category, rent, available, pic)
-VALUES('1625 Gaylord Dr, Austin, TX 78728', 'house', 1000, true, 'https://16b4xb3m2i8y3oxuu2aw0wmx-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/pasted-image-0-6.png');
