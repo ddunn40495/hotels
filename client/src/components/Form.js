4325e156e87d6892e10846d7c1f9f1d3a58f7f22
jaslone <jaredmslone@gmail.com>
2020-10-18 13:29:01 -0500

    form updated

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,8 +3,14 @@ import Input from './Input.js'
 
 class Form extends Component {
   state = {
-    name: '',
-    age: ''
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    rent: '',
+    pic: '',
+    available: '',
+    category: ''
   }
 
   handleChange = event => {
@@ -14,13 +20,24 @@ class Form extends Component {
   handleSubmit = event => {
     event.preventDefault()
     this.props.handleSubmit(event, {
-      name: this.state.name,
-      age: this.state.age,
-      id: this.state.id
+      address: this.state.address,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip,
+      rent: this.state.rent,
+      pic: this.state.pic,
+      available: this.state.available,
+      category: this.state.category,
     })
     this.setState({
-      name: '',
-      age: ''
+      address: '',
+      city: '',
+      state: '',
+      zip: '',
+      rent: '',
+      pic: '',
+      available: '',
+      category: ''
     })
     if (this.props.listing) {
       this.props.toggleForm()
@@ -31,21 +48,66 @@ class Form extends Component {
       <form onSubmit={this.handleSubmit}>
         <Input
           handleChange={this.handleChange}
-          name={'name'}
-          placeholder={'Name'}
-          title={'Name'}
+          name={'address'}
+          placeholder={'Address'}
+          title={'Address'}
           type={'text'}
-          value={this.state.name}
-          id={'name'}
+          value={this.state.address}
+          id={'address'}
         />
         <Input
           handleChange={this.handleChange}
-          name={'age'}
-          placeholder={'Age'}
-          title={'Age'}
+          name={'city'}
+          placeholder={'City'}
+          title={'City'}
           type={'number'}
-          value={this.state.age}
-          id={'age'}
+          value={this.state.city}
+          id={'city'}
+        />
+        <Input
+          handleChange={this.handleChange}
+          name={'state'}
+          placeholder={'State'}
+          title={'State'}
+          type={'text'}
+          value={this.state.state}
+          id={'state'}
+        />
+        <Input
+          handleChange={this.handleChange}
+          name={'zip'}
+          placeholder={'Zip'}
+          title={'Zip'}
+          type={'text'}
+          value={this.state.zip}
+          id={'zip'}
+        />
+        <Input
+          handleChange={this.handleChange}
+          name={'pic'}
+          placeholder={'Pic'}
+          title={'Pic'}
+          type={'text'}
+          value={this.state.pic}
+          id={'pic'}
+        />
+        <Input
+          handleChange={this.handleChange}
+          name={'available'}
+          placeholder={'Available'}
+          title={'Available'}
+          type={'text'}
+          value={this.state.available}
+          id={'available'}
+        />
+        <Input
+          handleChange={this.handleChange}
+          name={'category'}
+          placeholder={'Category'}
+          title={'Category'}
+          type={'text'}
+          value={this.state.category}
+          id={'category'}
         />
         <input
           type="submit"
