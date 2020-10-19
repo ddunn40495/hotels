0d6f849770d52fa48c05ecf5a6b86ad84e690e0e
jaslone <jaredmslone@gmail.com>
2020-10-19 15:25:22 -0500

    dropdown working

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { NavDropdown } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import Form from './Form.js';
+
 export default function Header(props) {
     return (
       <Navbar bg="light" expand="lg">
@@ -13,10 +14,8 @@ export default function Header(props) {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <NavDropdown title="Add new Listing" id="basic-nav-dropdown">
-          <NavDropdown.Item >
-            <div>
-              <Form handleSubmit={props.handleSubmit}/>
-            </div>
+          <NavDropdown.Item>
+            <Form handleSubmit={props.handleSubmit}/>
               </NavDropdown.Item>
         </NavDropdown>
       </Nav>
