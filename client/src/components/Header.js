68cff5bd616361bc7de552598bc88c341c835ae9
jaslone <jaredmslone@gmail.com>
2020-10-19 10:00:40 -0500

    cards and carddeck

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { NavDropdown } from 'react-bootstrap';
